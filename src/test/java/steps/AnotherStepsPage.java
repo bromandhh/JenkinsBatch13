968f3bf01116e3a9788fc731305705668b95883c
Hamid Bromand <bromandhh@gmail.com>
2022-10-06 10:56:39 -0700

    First Commit

---
+++ src/test/java/steps/AnotherStepsPage.java
@@ -0,0 +1,13 @@
+package steps;
+
+import io.cucumber.java.en.When;
+import utils.CommonMethods;
+
+public class AnotherStepsPage extends CommonMethods {
+
+    @When("user enters invalid username and password")
+    public void user_enters_invalid_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
